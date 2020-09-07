00c2252b0090cdc81e385475b048f4f323805dfb
Wotusay <wout.salembier@hotmail.com>
2020-09-07 13:36:43 +0200

    added a cube that spins

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import './style.css';
+import 'three';
 
-console.log(`Hey, ik ben een JS file`);
-const arr = [1, 2, 3];
-const ES6Stuff = () => console.log(`Ik kan ES6 stuff aan`, ...arr);
-ES6Stuff();
+const THREE = require('three');
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const color1 = new THREE.Color( 'skyblue' );
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    scene.background = color1;
+
+    renderer.render( scene, camera );
+};
+
+animate();
