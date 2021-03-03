356fe4b5c8aa864e4c24adc32fb9363b568473d1
wotusay <wout.salembier@hotmail.com>
2021-03-03 16:34:00 +0100

    Added physics

--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
 import * as THREE from 'three';
 import { fragmentShader } from './shaders/fragment.js';
-import { vertexShader } from './shaders/vertex.js'
+import { vertexShader } from './shaders/vertex.js';
+import * as dat from 'dat.gui';
 import './style.css';
+
+
 const OrbitControls = require('three-orbitcontrols');
+
+import * as OIMO from 'oimo';
+
+
 
 export default class Sketch {
   constructor(selector) {
     this.renderer = new THREE.WebGLRenderer( { antialias: true } );
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setClearColor(0xeeeeee, 1);
+    this.renderer.setClearColor(0x444444, 1);
+
     this.container = document.getElementById('container');
     this.container.appendChild( this.renderer.domElement );
     this.width =  this.container.offsetWidth;
     this.height = this.container.offsetHeight;
 
     this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.001, 1000 );
-    this.camera.position.set(0, 0, 2);
+    this.camera.position.set(0, 0, 6);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.scene = new THREE.Scene();
     this.setupResize();
+    this.physics();
     this.addMesh();
     this.time = 0;
     this.resize();
     this.render();
+    this.settings();
+
   }
 
   setupResize() {
     window.addEventListener('resize', this.resize.bind(this))
+  }
+
+  settings(){
+    let that = this;
+    this.settings = {
+      time: 0,
+      createBody: function() {
+        that.createBody();
+      }
+    };
+
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, "time", 0,100,0.01);
+    this.gui.add(this.settings, "createBody");
+
+    console.log()
   }
 
   resize() {
@@ -40,7 +67,7 @@ export default class Sketch {
 
   addMesh() {
     this.geometry = new THREE.PlaneBufferGeometry(1,1);
-    this.geometry = new THREE.OctahedronBufferGeometry(1)
+    this.geometry = new THREE.BoxBufferGeometry(1,1,1,1)
     this.material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
     this.material = new THREE.ShaderMaterial({
       fragmentShader: fragmentShader,
@@ -54,15 +81,79 @@ export default class Sketch {
       },
       side: THREE.DoubleSide
     })
-    this.mesh = new THREE.Mesh( this.geometry, this.material );
-    this.scene.add( this.mesh );
+    this.Object = new THREE.Mesh( this.geometry, this.material );
+    this.scene.add( this.Object );
+  }
+
+  physics() {
+    this.bodies = [];
+
+    this.world = new OIMO.World({
+      timestep: 1/60,
+      iterations: 8,
+      broadphase: 2, // 1 brute force, 2 sweep and prune, 3 volume tree
+      worldscale: 1, // scale full world
+      random: true,  // randomize sample
+      info: false,   // calculate statistic or not
+      gravity: [0,-9.8,0]
+    });
+
+    this.body = this.world.add({
+      type:'box', // type of shape : sphere, box, cylinder
+      size:[1,1,1], // size of shape
+      pos:[0,0,0], // start position in degree
+      rot:[0,0,90], // start rotation in degree
+      move:true, // dynamic or statique
+      density: 1,
+      friction: 0.2,
+      restitution: 0.2,
+      belongsTo: 1, // The bits of the collision groups to which the shape belongs.
+      collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.
+      });
+
+
+    this.ground = this.world.add({size:[40,1,40], pos: [0,-4.5,0]})
   }
 
 
+  createBody() {
+    let o = {};
+    let body = this.world.add({
+      type:'box', // type of shape : sphere, box, cylinder
+      size:[1,1,1], // size of shape
+      pos:[0,0,0], // start position in degree
+      rot:[0,0,90], // start rotation in degree
+      move:true, // dynamic or statique
+      density: 1,
+      friction: 0.2,
+      restitution: 0.2,
+      belongsTo: 1, // The bits of the collision groups to which the shape belongs.
+      collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.
+      });
+
+      let mesh = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(1),
+        new THREE.MeshBasicMaterial({color:0xff0000})
+      );
+
+
+      o.body = body;
+      o.mesh = mesh;
+
+      this.scene.add(mesh)
+
+      this.bodies.push(o)
+  }
 
   render() {
     this.time++;
-
+    this.world.step();
+    this.Object.position.copy( this.body.getPosition());
+    this.Object.quaternion.copy( this.body.getQuaternion());
+    this.bodies.forEach(b => {
+      b.mesh.position.copy( b.body.getPosition());
+      b.mesh.quaternion.copy( b.body.getQuaternion());
+    })
     this.renderer.render( this.scene, this.camera );
     window.requestAnimationFrame(this.render.bind(this));
   }
--- src/shaders/fragment.js
+++ src/shaders/fragment.js
@@ -9,6 +9,14 @@ varying vec4 vPosition;
 varying vec2 vUv;
 
 void main() {
-  gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+
+  float width = 0.01;
+
+  float borderx = max(step(vUv.x, width),step(1.0 - vUv.x,width));
+  float bordery = max(step(vUv.y, width),step(1.0 - vUv.y,width));
+  float border = max(borderx,bordery);
+
+  vec3 color = vec3(border);
+  gl_FragColor = vec4(color,1.0);
 }
 `;
